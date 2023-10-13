18d241b3d0ba0788a920fa5e25a15d802e0a8c1d
DavidKUMARDev <davkumarpro@gmail.com>
2023-10-13 11:00:57 +0200

    requete corrigé

--- lib/home_page/home_controller.dart
+++ lib/home_page/home_controller.dart
@@ -14,7 +14,9 @@ class HomeController extends GetxController {
 //https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=1&where="69330"
   int currentIndex = 0;
 
-  final queryParameters = {'limit': '20','order_by':'"gazole_prix ASC"'}; //gazole
+  final queryParameters = {'select':'id, cp, adresse, ville, gazole_prix, e10_prix, sp98_prix, departement, code_departement, region, code_region',
+  'limit': '20',
+  'order_by':'gazole_prix ASC'}; //gazole
  //final queryParameters = {'limit': '20','order_by':'"e10_prix ASC"'}; //95 + 10%ethanol
  //final queryParameters = {'limit': '20','order_by':'"e85_prix ASC"'};// ethanol
  //final queryParameters = {'limit': '20','order_by':'"gplc_prix ASC"'}; // gpl
