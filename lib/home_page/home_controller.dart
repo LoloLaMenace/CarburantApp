858e1abc0a905fd52e72417fbd392b2850b83335
DavidKUMARDev <davkumarpro@gmail.com>
2023-10-13 09:44:34 +0200

    api parametres

--- lib/home_page/home_controller.dart
+++ lib/home_page/home_controller.dart
@@ -14,7 +14,15 @@ class HomeController extends GetxController {
 //https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?limit=1&where="69330"
   int currentIndex = 0;
 
-  final queryParameters = {'limit': '1','where':'"69330"'};
+  final queryParameters = {'limit': '20','order_by':'"gazole_prix ASC"'}; //gazole
+ //final queryParameters = {'limit': '20','order_by':'"e10_prix ASC"'}; //95 + 10%ethanol
+ //final queryParameters = {'limit': '20','order_by':'"e85_prix ASC"'};// ethanol
+ //final queryParameters = {'limit': '20','order_by':'"gplc_prix ASC"'}; // gpl
+ //final queryParameters = {'limit': '20','order_by':'"sp98_prix ASC"'}; // sp98
+
+
+
+
   Future<void> getPrice() async {
     var url = Uri.https(
         'data.economie.gouv.fr',
